d2730a87f54762c2b347625fcbb110cfc68c2f25
Hernan Lopez <hernan.lopezgonzalez@externos-cl.cencosud.com>
2022-07-13 12:39:03 -0400

    creación endpoint consultar usuario especifico

--- pruebatecnica/src/main/java/com/bci/prueba/tecnica/pruebatecnica/controller/User.java
+++ pruebatecnica/src/main/java/com/bci/prueba/tecnica/pruebatecnica/controller/User.java
@@ -3,14 +3,19 @@ package com.bci.prueba.tecnica.pruebatecnica.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bci.prueba.tecnica.pruebatecnica.domain.request.UserRQ;
@@ -19,14 +24,14 @@ import com.bci.prueba.tecnica.pruebatecnica.domain.response.UserListRS;
 import com.bci.prueba.tecnica.pruebatecnica.domain.response.UserRS;
 
 @RestController
-@RequestMapping(value = "/usuario")
+@RequestMapping("/usuario")
 public class User {
 
 	private static final Logger log = LoggerFactory.getLogger(User.class);
 
-	@GetMapping
-	public ResponseEntity<UserListRS> getAllUser() {
-		log.info("[INIT][getAllUser]");
+	@GetMapping(value = "/get-all", produces = { MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<UserListRS> getAll() {
+		log.info("[INIT][getAll]");
 
 		ResponseEntity<UserListRS> response = null;
 		UserListRS userListRS = new UserListRS();
@@ -41,12 +46,31 @@ public class User {
 			userListRS.setMensaje("Error interno, intente más tarde");
 			response = new ResponseEntity<>(userListRS, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		log.info("[END][insertUser]");
+		log.info("[END][getAll]");
 		return response;
 	}
 
-	@PostMapping
-	public ResponseEntity<ProcessRS> insertUser(@RequestBody UserRQ rq) {
+	@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<UserRS> getByEmail(@NotNull @NotBlank @RequestParam String email) {
+		log.info("[INIT][getByEmail]");
+		log.info("[PARAMS] " + email);
+
+		ResponseEntity<UserRS> response = null;
+		UserRS userRS = new UserRS();
+		try {
+
+			response = new ResponseEntity<>(userRS, HttpStatus.OK);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			userRS.setMensaje("Error interno, intente más tarde");
+			response = new ResponseEntity<>(userRS, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		log.info("[END][getByEmail]");
+		return response;
+	}
+
+	@PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<ProcessRS> save(@RequestBody UserRQ rq) {
 		log.info("[INIT][insertUser]");
 		log.info("[PARAMS] " + rq);
 
